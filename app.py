f83e06b0c97f2ebe0b451a29b4ac39c7780ce4e8
mmiguelcosta <miguelccpereira34@gmail.com>
2024-10-25 10:35:50 -0300

    fds

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Senha'
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/bim3g1"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+from database import db
+from flask_migrate import Migrate
+db.init_app(app)
+migrate = Migrate(app, db)
+from models import Usuario, Pizza, Pedido
---
+++ modulos/usuarios.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template, request, redirect, flash
+from database import db
+from models import Usuario
+
+bp_usuario = Blueprint('usuarios', __name__, template_folder='templates')
+
+@bp_usuario.route('/')
+def index():
+    dados = Usuario.query.all()
+    return render_template('usuario.html', dados=dados)
+
+@bp_usuario.route('/add')
+def add():
+    return render_template('usuario_add.html')
+
+@bp_usuario.route('/save', methods=['POST'])
+def save():
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    senha = request.form.get('senha')
+    if nome and email and senha:
+        objeto = Usuario(nome, email, senha)
+        db.session.add(objeto)
+        db.session.commit()
+        flash('Usuario salvo')
+        return redirect('/')
+    else:
+        flash('Preencha todos os campos')
+        return redirect('/add')
+
+
